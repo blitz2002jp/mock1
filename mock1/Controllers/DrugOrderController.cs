deb3149c1de750a6cfa589155fdc86edf811d007
oeda <m.ooeda@eisan.co.jp>
2018-12-04 11:43:46 +0900

    first

---
+++ mock1/Controllers/DrugOrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mock1.Controllers
+{
+    public class DrugOrderController : Controller
+    {
+        // GET: DrugOrder
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
